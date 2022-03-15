2bb396c0813d6c34375447c85c6544adc77806de
4Karolis <4karolis.t@gmail.com>
2022-03-15 19:38:17 +0200

    Update Program.cs

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -8,13 +8,10 @@ namespace GitProject
         {
             Console.WriteLine("Svx");
             Console.WriteLine("Pridejau dar viena eiluite");
-<<<<<<< HEAD
             Console.WriteLine("Feature branch 2");
             Console.WriteLine("changes from feature branch 2");
-=======
             Console.WriteLine("Feature branch 3");
             Console.WriteLine("CHamnges from feature branch 3");
->>>>>>> Feature-branch-3
         }
     }
 }
